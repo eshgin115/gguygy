d4b5b021658a4bf6f8839805dad4476bf10fec10
unknown <you@example.com>
2022-05-29 07:05:51 -0700

    yenilik

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,135 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int azn = 34000;
+
+
+float usd = 0.588f;
+
+float evro = 0.54f;
+
+
+float rubl = 38.7f;
+
+
+string name = "34k manats how many dollars";
+  Console.WriteLine(name);
+
+ float hesab = azn * usd;
+Console.WriteLine(hesab);
+
+string name2 = "34k manats how many evro";
+Console.WriteLine(name2);
+
+
+
+float hesab2 = azn * evro; 
+Console.WriteLine(hesab2);
+
+
+
+string name3 = "34k manats how many rubl";
+
+Console.WriteLine(name3);
+
+
+float hesab3 = azn * rubl;
+
+Console.WriteLine(hesab3);
+
+
+Console.WriteLine();
+
+
+string name4 = "budget";
+
+Console.WriteLine(name4);
+
+
+float budget = 400000f;
+
+
+Console.WriteLine(budget);
+
+string name5 = "MD";
+
+    Console.WriteLine(name5);
+
+
+float MD =0.2f;
+float orta = MD * budget;
+Console.WriteLine(orta);
+
+
+
+
+
+string name6 = "MT ";
+Console.WriteLine(name6);
+
+float MT = 0.1f;
+float orta2 = MT * budget;
+Console.WriteLine(orta2);
+
+string name7 = "balance of the budget";
+Console.WriteLine(name7);
+
+float btb =budget-(orta+orta2);   
+Console.WriteLine(btb);
+
+
+
+
+Console.WriteLine();
+
+
+string name8 = "cargo program";
+Console.WriteLine(name8);
+
+
+string name9 = "adidas";
+Console.WriteLine(name9);
+float marka = 100f;
+float topcost = marka * 4;
+
+Console.WriteLine(topcost);
+
+
+string name10 = "nike";
+
+
+
+Console.WriteLine(name10);
+
+
+float marka2 = 500f;
+
+float topcost2 = marka2 * 2;
+Console.WriteLine(topcost2);
+
+
+string name11 = "chanel";
+Console.WriteLine(name11);
+float marka3 = 75f;
+
+
+ float topcost3 = marka3 * 1;
+Console.WriteLine(topcost3);
+
+
+
+string total = "all price";
+    Console.WriteLine(total);
+float all =topcost + topcost2 +  topcost3;
+Console.WriteLine(all);
+
+
+            
+ 
+
+
+
+
+
+
+
+
